c823c7c74d078fb131c539aab424eabc49770fe3
alexcutschall <alexander.cutschall@gmail.com>
2018-03-27 13:24:40 -0600

    Adding in the test for seeing an individual student's page

---
+++ spec/features/students/user_can_see_an_individual_student_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "A user can see a student's page" do
+  context "when the user visits a specific students page" do
+    it "shows only that students information" do
+      student = Student.create(name: "Alex")
+      visit "/students/#{student.id}"
+
+      expect(page).to have_content(student.name)
+    end
+  end
+end
