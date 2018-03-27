ccafa8ee965a3c214cb343848ffbd669f5fd807c
alexcutschall <alexander.cutschall@gmail.com>
2018-03-27 15:49:34 -0600

    adding a test to ensure that the correct address info is added to student page

--- spec/features/students/user_can_see_an_individual_student_spec.rb
+++ spec/features/students/user_can_see_an_individual_student_spec.rb
@@ -8,5 +8,23 @@ describe "A user can see a student's page" do
 
       expect(page).to have_content(student.name)
     end
+    context "user can see all that student's addresses" do
+      it "the student's information" do
+        student = Student.create!(name: "Alex")
+        student_2 = Student.create!(name: "Jimmy")
+        address = student.addresses.create(description: "Awesome",
+                              street: "123",
+                              city: "Denver",
+                              state: "CO",
+                              zip_code: "55011")
+        visit "/students/#{student.id}"
+
+        expect(page).to have_content(address.description)
+        expect(page).to have_content(address.street)
+        expect(page).to have_content(address.city)
+        expect(page).to have_content(address.state)
+        expect(page).to have_content(address.zip_code)
+      end
+    end 
   end
 end
