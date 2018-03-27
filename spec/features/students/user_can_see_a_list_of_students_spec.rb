3a2ea8843f812f7bace0eee48acbb0f6bd6f0876
alexcutschall <alexander.cutschall@gmail.com>
2018-03-27 13:39:52 -0600

    Adding in failing test for students index

--- spec/features/students/user_can_see_a_list_of_students_spec.rb
+++ spec/features/students/user_can_see_a_list_of_students_spec.rb
@@ -1,3 +1,14 @@
 require "rails_helper"
 
-describe "User visits an index page"
+describe "User visits an index page" do
+  it "shows a list of students" do
+    student_1 = Student.new(name: "Alex")
+    student_2 = Student.new(name: "Jimmy")
+    student_3 = Student.new(name: "Megan")
+    visit '/students'
+
+    expect(page).to have_content(student_1.name)
+    expect(page).to have_content(student_2.name)
+    expect(page).to have_content(student_3.name)
+  end
+end
